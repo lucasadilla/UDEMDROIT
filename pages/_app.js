30bd13c0401783e9898f268f7a3386d55b96b16f
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-02-04 22:59:27 -0500

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import { ArticlesProvider } from '../context/ArticlesContext';
 import '../styles/global.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
     return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Analytics } from "@vercel/analytics/react"
 
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="fr">
+        <Html lang="fr">
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
                 </Head>
