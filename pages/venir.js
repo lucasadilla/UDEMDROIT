8220c0b0b88dda2c8e85bdc15208843eab1d67c7
Lucas Pentland-Hyde <lucas.pentlandhyde@gmail.com>
2025-02-05 18:49:11 -0500

    added a calendar

--- pages/venir.js
+++ pages/venir.js
@@ -5,23 +5,45 @@ import Head from "next/head";
 import React from "react";
 import SponsorsBar from "../components/Sponsors";
 
-
-
 export default function Venir() {
     return (
         <div>
             <Head>
                 <title>Événements à Venir</title>
-                <meta name="description" content="Restez informé des événements à venir organisés par Femme & Droit à l'Université de Montréal." />
-                <meta name="keywords" content="événements, à venir, féminisme, Université de Montréal, communauté" />
+                <meta
+                    name="description"
+                    content="Restez informé des événements à venir organisés par Femme & Droit à l'Université de Montréal."
+                />
+                <meta
+                    name="keywords"
+                    content="événements, à venir, féminisme, Université de Montréal, communauté"
+                />
             </Head>
             <Navbar />
             <main className="p-8">
                 <h1 className="page-title text-center mb-8">ÉVÉNEMENTS À VENIR</h1>
-                <img  src="/images/calendar.png" className= "calendar"/>
+
+                {/* Desktop version */}
+                <div className="hidden md:block">
+                    <img
+                        src="/images/Feb.png" // Replace with your desktop PNG path
+                        alt="Calendar for Desktop"
+                        className="calendar w-full"
+                    />
+                </div>
+
+                {/* Mobile version */}
+                <div className="block md:hidden">
+                    <img
+                        src="/images/FebPhone.png" // Replace with your mobile PNG path
+                        alt="Calendar for Mobile"
+                        className="calendar w-full"
+                    />
+                </div>
+
                 <SponsorsBar />
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
