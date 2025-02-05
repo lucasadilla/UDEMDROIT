5e9c40ea39bfbb7b948de1b7e97e075f056bbe67
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-02-05 00:03:35 -0500

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,13 @@ class MyDocument extends Document {
     render() {
         return (
         <Html lang="fr">
-                <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                <Main />
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+                      rel="stylesheet"/>
+                <link rel="icon" href="/favicon.PNG"/>
+            </Head>
+            <body>
+            <Main />
                 <NextScript />
                 </body>
             </Html>
